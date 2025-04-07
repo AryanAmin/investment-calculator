cea69a87558e36798fbb7647d13fd632c1c62114
Aryan Amin <amin5300@mylaurier.ca>
2025-04-06 21:51:01 -0400

    Added user components and state logic

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleUserInput(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleUserInput("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) => {
+              handleUserInput("annualInvestment", event.target.value);
+            }}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) => {
+              handleUserInput("expectedReturn", event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => {
+              handleUserInput("duration", event.target.value);
+            }}
+          />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default UserInput;
